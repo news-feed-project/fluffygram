1ce4fc8c82b92475423e8b85361ecf91dd94d737
j-hann <j.han77055@gmail.com>
2024-11-20 11:54:48 +0900

    🎉 Begin : 프로젝트 시작
    프로젝트 생성 완료

---
+++ src/main/java/com/fluffygram/newsfeed/FluffygramProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fluffygram.newsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluffygramProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluffygramProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fluffygram/newsfeed/FluffygramProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluffygram.newsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluffygramProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
