24e3493f358979fa116147110c159455ff87d837
taketheking <so4524@gmail.com>
2024-11-24 00:40:49 +0900

    🔧 Add : 이미지 경로를 mac과 window 모두 허용할 수 있도록 변경함

--- src/main/java/com/fluffygram/newsfeed/global/config/Const.java
+++ src/main/java/com/fluffygram/newsfeed/global/config/Const.java
@@ -6,11 +6,22 @@ import java.nio.file.Paths;
 public abstract class Const {
     public static final String LOGIN_USER = "loginUser";
 
-    public static final String USER_IMAGE_STORAGE = "C:/fluffygram/user/image";
+    public static String USER_IMAGE_STORAGE;
+
+    public static String BOARD_IMAGE_STORAGE;
+
+
+    static {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")){
+            USER_IMAGE_STORAGE = "C:/fluffygram/user/image";
+            BOARD_IMAGE_STORAGE = "C:/fluffygram/board/image";
+        }else{
+            USER_IMAGE_STORAGE = "/fluffygram/user/image";
+            BOARD_IMAGE_STORAGE = "/fluffygram/board/image";
+        }
+    }
 
     public static final Path USER_IMAGE_STORAGE_PATH = Paths.get(USER_IMAGE_STORAGE);
-
-    public static final String BOARD_IMAGE_STORAGE = "C:/fluffygram/board/image";
-
     public static final Path BOARD_IMAGE_STORAGE_PATH = Paths.get(BOARD_IMAGE_STORAGE);
 }
